12d33649fec858eaa4b804db92684ede87d37950
pzarczynski <piotr.zarczynski.06@gmail.com>
2026-01-07 13:29:13 +0100

    fix memory script and alacritty font

--- .config/waybar/scripts/memory_usage.sh
+++ .config/waybar/scripts/memory_usage.sh
@@ -3,23 +3,25 @@
 # Read values from /proc/meminfo
 while read -r key value _; do
   case "$key" in
-    MemTotal:)       total=$value ;;
-    MemFree:)        free=$value ;;
-    "Active(file):")     active_file=$value ;;
-    "Inactive(file):")   inactive_file=$value ;;
-    SReclaimable:)   sreclaimable=$value ;;
+    MemTotal:)        total=$value ;;
+    MemFree:)         free=$value ;;
+    Active\(file\):)  active_file=$value ;;
+    Inactive\(file\):) inactive_file=$value ;;
+    SReclaimable:)    sreclaimable=$value ;;
   esac
 done < /proc/meminfo
 
 # htop-style used memory (kB)
 used=$(( total - (free + active_file + inactive_file + sreclaimable) ))
+percentage=$(( (100 * used) / total ))
 
 # Convert to MB/GB
 if (( used < 1048576 )); then
-  mem_str="$(( used / 1024 ))MiB"
+  mem_str="$(( used / 1024 ))MiB ($percentage%)"
 else
-  mem_str="$(awk -v val=$used 'BEGIN {printf "%.1fGiB", val/1048576}')"
+  mem_str="$(awk -v used=$used -v total=$total \
+      'BEGIN { printf "%.1f/%.1fGiB", used/1048576, total/1048576 }') ($percentage%)"
 fi
 
-# Output JSON for Waybar
-echo "{\"text\": \"$mem_str\"}"
+echo {\"text\": \"$mem_str\"}
+
